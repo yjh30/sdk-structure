c3c92eed6963bce38e9ec532fa2a9317ed377c9b
重华-杨君华 <chonghua@tsign.cn>
2019-07-25 14:47:55 +0800

    update

--- packages/sdk-structure/package/index.js
+++ packages/sdk-structure/package/index.js
@@ -2,14 +2,11 @@
 
 import moduleA from '@esign/module-a';
 import moduleB from '@esign/module-b';
-import moduleC from '@esign/module-c';
 
 exports.moduleA = moduleA;
 exports.moduleB = moduleB;
-exports.moduleC = moduleC;
 
 export default {
   moduleA,
   moduleB,
-  moduleC,
 };
